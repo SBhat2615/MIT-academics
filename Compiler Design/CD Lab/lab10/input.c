45d9cab9bc421cac9b9918df82a2a66f9e57c536
SBhat2615 <siddharthabhat09@gmail.com>
2022-11-01 15:23:11 +0530

    1 Nov

---
+++ Compiler Design/CD Lab/lab10/input.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+INT MAIN(){
+  int n1, n2;
+  SCANF("Enter number 1: %d", &n1);
+  PRINTF("Number 1: %d", n1);
+  //scanf("Enter number 2: %d", &n2);
+  //printf("Number 2: %d", n2);
+  //printf("Done !!");
+}
+/*
+THIS IS A COMMENT
+*/
